094a96b956baf82956e156fb3c6ebde7f578dab5
marina <marinasabha3@gmail.com>
2018-03-30 13:37:34 +0300

    test page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,11 @@
 $user =Auth::user()->name;
-
+@if(count($user) > 0)
+<ul>
+foreach($user->all() as $users)
 <div class="content">
     <div class="title m-b-md">
-        {{$user}}
+    <li>    {{$users}} </li>
     </div>
+    @endforeach
+  </ul>
+@endif
